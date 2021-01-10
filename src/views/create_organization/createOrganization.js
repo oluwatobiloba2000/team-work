21b1b4e579dcf6736bff8f07cb2531ac47fade6c
unknown <ananioluwatobiloba2000@gmail.com>
2021-01-10 01:18:36 +0100

    built create organization page

---
+++ src/views/create_organization/createOrganization.js
@@ -0,0 +1,281 @@
+import React, { useEffect, useState } from 'react'
+import './createOrganization.css';
+import { Container, Header, Content, Sidebar, Placeholder, Whisper, Tooltip, ButtonToolbar, Button, InputPicker, Avatar, Icon, Notification, Loader } from 'rsuite';
+import Logo from '../../components/logo/logo';
+import { Form, FormGroup, FormControl, ControlLabel, HelpBlock, Schema } from 'rsuite';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+const { Paragraph } = Placeholder;
+
+const images = [
+    "https://res.cloudinary.com/oluwatobby/image/upload/v1608737189/doodle_choice_8_khmtou.jpg",
+    "https://res.cloudinary.com/oluwatobby/image/upload/v1608737189/doodle_choice_4_q5eeu5.jpg",
+    "https://res.cloudinary.com/oluwatobby/image/upload/v1608737189/doodle_choice_3_guwyfg.jpg",
+    "https://res.cloudinary.com/oluwatobby/image/upload/v1608737189/doodle_choice_5_crpxzq.png",
+    "https://res.cloudinary.com/oluwatobby/image/upload/v1608737190/doodle_choice_2_zvwvti.jpg",
+    "https://res.cloudinary.com/oluwatobby/image/upload/v1608737190/doodle_choice_6_xea4wk.png",
+    "https://res.cloudinary.com/oluwatobby/image/upload/v1608737190/doodle_choice_1_qxkvzf.png",
+    "https://res.cloudinary.com/oluwatobby/image/upload/v1608737242/doodle_choice_7_q7omlz.jpg"
+]
+
+export default function CreateOrganization(props) {
+    const { StringType } = Schema.Types;
+    const model = Schema.Model({
+        name: StringType()
+            .isRequired('Name is required.')
+            .minLength(4, "Name Length should more than 4 characters long")
+            .maxLength(35, "Name should not exceed 35 characters"),
+        description: (StringType()
+            .isRequired('Description field is required.'))
+            .maxLength(250, "Description should not exceed 250 characters, please keep it short"),
+    });
+    const [loading, setLoading] = useState({
+        loading: false,
+        message: ''
+    });
+    const [formValues, setFormValues] = useState({
+        name: '',
+        isPrivate: true,
+        previewOrgImg: '',
+        orgimgFile: '',
+        description: '',
+        headerImg: ''
+    })
+
+    // const [orgImg, setOrgImg] = useState('')
+
+    const createOrganization = (orgImg) =>{
+        // if(!orgImg) return Notification["error"]({
+        //     title: 'Image Url Error',
+        //     description: "Image Url not avaliable"
+        // })
+        axios.post('/org/create', {
+            name: formValues.name,
+            orgImg: orgImg,
+            description: formValues.description,
+            headerImg: formValues.headerImg,
+            isPrivate: formValues.isPrivate
+        })
+        .then(function (response) {
+            setLoading({loading: false, message: ''});
+            console.log({'form values before creating the orgnization': formValues})
+            console.log("🚀 ~ file: createOrganization.js ~ line 54 ~ response", response)
+            // handle success
+            if(response.status === 200){
+              Notification["success"]({
+                 title: 'upload successful',
+                 description: "organization created successfully"
+              })
+            }
+            setTimeout(()=>{
+                props.history.push(`/organization/${response.data.data.organization[0].id}`)
+              }, 2000)
+            })
+            .catch(function (error) {
+              console.log("🚀 ~ file: view_organizations.js ~ line 32 ~ useEffect ~ error", error)
+              setLoading({loading: false, message: ''})
+              if(error.message.indexOf('403') !== -1){
+                Notification['error']({
+                  title: 'Authentication Error',
+                  description: 'You have to login'
+                });
+                setTimeout(()=>{
+                  props.history.push('/auth/login')
+                }, 2000)
+              }else if(error.message.indexOf('Network Error') !== -1){
+                Notification['warning']({
+                  title: 'Network Error',
+                  description: 'Looks Like you are not connected to the internet'
+                  });
+              }else{
+                Notification['warning']({
+                  title: 'Server Error',
+                  description: 'Something went wrong'
+                });
+              }
+            });
+    }
+
+    const handleSubmit = (data) => {
+        if(data){
+            // upload image
+            setLoading({
+                loading: true,
+                message: 'Uploading Organization Image ....'
+            })
+            if(formValues.orgimgFile){
+                const formData = new FormData();
+                formData.append('image', formValues.orgimgFile);
+
+                axios.post('/upload', formData, {
+                    headers: {
+                        'content-type': 'multipart/form-data'
+                    }
+                })
+                .then(function (response) {
+                  console.log("🚀 ~ file: createOrganization.js ~ line 110 ~ response", response)
+                  // handle success
+                  if(response.status === 200){
+                    // setOrgImg(response.data.data.secure_url)
+
+                    console.log("form values after uploading", formValues, {url: response.data.data.secure_url})
+                    Notification["success"]({
+                       title: 'upload successful',
+                       description: "organization profile Image has been uploaded"
+                    })
+                    createOrganization(response.data.data.secure_url)
+                  }
+                  })
+                  .catch(function (error) {
+                    console.log("🚀 ~ file: view_organizations.js ~ line 32 ~ useEffect ~ error", error)
+                    setLoading({loading: false, message: ''})
+                    if(error.message.indexOf('403') !== -1){
+                      Notification['error']({
+                        title: 'Authentication Error',
+                        description: 'You have to login'
+                      });
+                      setTimeout(()=>{
+                        props.history.push('/auth/login')
+                      }, 2000)
+                    }else if(error.message.indexOf('Network Error') !== -1){
+                      Notification['warning']({
+                        title: 'Network Error',
+                        description: 'Looks Like you are not connected to the internet'
+                        });
+                    }else{
+                      Notification['warning']({
+                        title: 'Server Error',
+                        description: 'Something went wrong'
+                      });
+                    }
+                  });
+            }else{
+                createOrganization();
+            }
+        }
+    }
+
+    const returnRandomHeaderPics = () => {
+        const randomNum = Math.floor(Math.random() * 8) + 1;
+        return images[randomNum];
+    }
+
+    const handleUploadImage = (e) => {
+        e.preventDefault();
+        if(e.target.files[0]){
+            const reader = new FileReader();
+            const file = e.target.files[0]
+            reader.onloadend = () => {
+                setFormValues({
+                ...formValues,
+                  orgimgFile: file,
+                  previewOrgImg: reader.result
+                });
+              }
+
+              reader.readAsDataURL(file)
+        }
+    }
+
+    useEffect(() => {
+        const authTokenFromLocalStorage = JSON.parse(window.localStorage.getItem('auth-token'));
+        if(authTokenFromLocalStorage){
+            axios.defaults.headers.common['Authorization'] = `Bearer ${authTokenFromLocalStorage}`;
+            setFormValues({ ...formValues, headerImg: returnRandomHeaderPics() });
+        }else{
+        Notification['error']({
+            title: 'Authentication Error',
+            description: 'You have to login'
+            });
+            setTimeout(()=>{
+            props.history.push('/auth/login')
+            }, 2000)
+        }
+        console.log({ formValues })
+    }, [])
+
+    return (
+        <Container className="create_organization_container">
+           {loading.loading && <Loader backdrop content={loading.message} vertical />}
+            <Header className="header">
+                <Logo className="org_logo" />
+            </Header>
+            <Container>
+                <Sidebar className="org_sidebar">
+                    <div className="org_dash_name">
+                        {formValues.name
+                            ? <Whisper placement="top" trigger="hover" speaker={<Tooltip>{formValues.name}</Tooltip>}>
+                                <p style={{ cursor: 'pointer' }}>{formValues.name}</p>
+                            </Whisper>
+                            : <Paragraph className="org_dash_name_loader" rows={1} />
+                        }
+                    </div>
+                    <div style={{ backgroundImage: `url(${formValues.headerImg})`, backgroundSize: "100% 100%" }} className="org_dash_panel_details">
+                        {formValues.previewOrgImg ? <Avatar className="org_profile_img org_profile_img_rounded" src={formValues.previewOrgImg} size="lg" />
+                            :
+                            <Paragraph className="org_profile_img" rows={0} style={{ marginTop: 30 }} graph="image" />}
+                        {formValues.isPrivate ? <Icon className="privacy_icon" icon="lock" /> : <Icon className="privacy_icon privacy_globe" icon="globe2" />}
+                      
+                    </div>
+
+                    <div className="org_dash_description">
+                        <p>Description</p>
+                        {formValues.description
+                        ? <div className="org_dash_description_text">{formValues.description}</div>
+                        :
+                         <Paragraph className="org_dash_name_loader" rows={1} />
+                       }
+                    </div>
+                </Sidebar>
+                <Content className="create_org_content org_content">
+                    <Form onSubmit={handleSubmit} onChange={(values) => setFormValues({...formValues, name: values.name, description: values.description })} model={model} className="create_org_form" fluid>
+                        <FormGroup>
+                            <ControlLabel className="create_org_label">What's the name of your Organization?</ControlLabel>
+                            <FormControl className="create_org_input" placeholder="Organization name" name="name" />
+                            <HelpBlock>choose a descriptive name</HelpBlock>
+                        </FormGroup>
+
+                        <FormGroup style={{ marginTop: '130px' }}>
+                            <ControlLabel className="create_org_label">Add more description about your Organization</ControlLabel>
+                            <FormControl className="create_org_input" name="description" />
+                        </FormGroup>
+
+                        <FormGroup style={{ marginTop: '130px' }}>
+                            <ControlLabel className="create_org_label">Add a profile image for your organization</ControlLabel>
+                             <div className="uploader_container">
+                              <input type="file" accept="image/x-png,image/gif,image/jpeg" onChange={(e)=> handleUploadImage(e)} />
+                             </div>
+                            {/* <FormControl ref={uploaderImg} className="create_org_input" onChange={handleUploadImage} name="orgImg" type="file" /> */}
+                        </FormGroup>
+
+                        {/* <FormGroup style={{ marginTop: '130px' }}>
+                            <ControlLabel className="create_org_label">Add an header image for your organization</ControlLabel>
+                            <FormControl className="create_org_input" name="headerImg" type="file" />
+                        </FormGroup> */}
+
+                        <FormGroup style={{ marginTop: '130px' }}>
+                            <ControlLabel className="create_org_label">Set Organization Privacy</ControlLabel>
+                            <InputPicker defaultValue={true} className="create_org_input" onSelect={(val) => setFormValues({...formValues, isPrivate: val})} name="privacy" data={[{
+                                value: true,
+                                label: 'private',
+                                groupBy: 'privacy'
+                            }, {
+                                value: false,
+                                label: 'public',
+                                groupBy: 'privacy'
+                            },
+                            ]} style={{ width: "100%" }} />
+                        </FormGroup>
+
+                        <FormGroup style={{ marginTop: '130px' }}>
+                            <ButtonToolbar style={{position: 'relative'}}>
+                                <Button loading={loading.loading} className="create_org_form_btn" type="submit" appearance="primary">Create Organization</Button>
+                                <Link style={{zIndex: '10'}} to="/user/organization"><Button appearance="ghost">Cancel</Button></Link>
+                            </ButtonToolbar>
+                        </FormGroup>
+                      </Form>
+                </Content>
+            </Container>
+        </Container>
+    )
+}
